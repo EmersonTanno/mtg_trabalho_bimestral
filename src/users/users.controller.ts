576213e5e82b99bed81baf951648dfa87778a5b2
Emerson Tanno <tannobrizotto@gmail.com>
2024-08-25 23:31:47 -0300

    users_complete_crud_and_routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Post, Body, Get } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, NotFoundException, ConflictException, Put, Delete } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { User } from './users.schema';
 
 @Controller('users')
 export class UsersController {
@@ -7,8 +8,39 @@ export class UsersController {
 
   @Post('create')
   async createUser(@Body('username') username: string, @Body('password') password: string) {
+    const existinguser = await this.usersService.findOne(username);
+    if(existinguser){
+      throw new ConflictException(`User "${username}" alredy exists`);
+    }
     const user = await this.usersService.createUser(username, password);
-    return { username: user.username }; 
+    return (`User "${user.username}" create sucess`);
+    
+  }
+
+  @Get(':username')
+  async getUser(@Param('username') username: string): Promise<Partial<User>> {
+    const user = await this.usersService.findOne(username);
+
+    if (!user) {
+      throw new NotFoundException(`User with username "${username}" not found`);
+    }
+
+    const { password, ...result } = user;
+    return result;
+  }
+
+  @Put(':username')
+  async updateUser(@Param('username') username: string, @Body('newUserName') newUserName: string, @Body('newPassword') newPassword: string): Promise<{username:string}>{
+    const updatedUser = await this.usersService.updateUser(username, newUserName, newPassword);
+
+    return { username: updatedUser.username };
+  }
+
+  @Delete(':username')
+  async deleteUser(@Param('username') username: string): Promise<{ username: string }> {
+    const deletedUser = await this.usersService.deleteUser(username);
+  
+    return { username: deletedUser.username };
   }
 
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import * as bcrypt from 'bcrypt';
 import { User, UserDocument } from './users.schema';
+import { NotificationKind } from 'rxjs';
 
 @Injectable()
 export class UsersService {
@@ -23,7 +24,44 @@ export class UsersService {
   }
 
   async findOne(username: string): Promise<User | undefined> {
-    return this.userModel.findOne({ username }).exec();
+    return this.userModel.findOne({ username }).lean().exec();
   }
+
+  async updateUser(username: string, newUserName: string, newPassword: string): Promise<User> {
+    const existingUser = await this.userModel.findOne({ username }).exec();
+    
+    if (!existingUser) {
+      throw new NotFoundException(`User "${username}" not found`);
+    }
+  
+    if (newUserName && newUserName !== username) {
+      const userWithNewUsername = await this.userModel.findOne({ username: newUserName }).exec();
+      if (userWithNewUsername) {
+        throw new ConflictException(`User with username "${newUserName}" already exists`);
+      }
+      existingUser.username = newUserName;
+    }
+  
+    if (newPassword) {
+      const salt = await bcrypt.genSalt();
+      const hashedPassword = await bcrypt.hash(newPassword, salt);
+      existingUser.password = hashedPassword;
+    }
+  
+    await existingUser.save();
+    return existingUser;
+  }
+
+  async deleteUser(username: string): Promise<User> {
+    const existingUser = await this.userModel.findOneAndDelete({ username }).exec();
+  
+    if (!existingUser) {
+      throw new NotFoundException(`User "${username}" not found`);
+    }
+  
+    return existingUser;
+  }
+  
+
 }
 
